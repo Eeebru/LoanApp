9333ce248a89df5d20e8e57866ce8d9eb14e93c2
Ibrahim Alao <alaoopeyemi101@gmail.com>
2020-11-13 19:48:03 +0100

    add condition render to take loan page

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {useEffect} from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 // import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-import './dashboard.css';
-import cardPic from './mastercard.jpg'
-import DashModal from './modal';
+import "./dashboard.css";
+import cardPic from "./mastercard.jpg";
+import DashModal from "./modal";
 
 const Dashboard = () => {
-   const [modalShow, setModalShow] = React.useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
+	const [showLoan, setShowLoan] = React.useState(false);
 
+  function simulateNetworkRequest() {
+		return new Promise((resolve) => {
+			setTimeout(resolve, 5000);
+		});
+	}
 
-  return (
+	useEffect(() => {
+		simulateNetworkRequest().then(() => setShowLoan(true));
+	}, []);
+	const simulateLoanHistory = [];
+
+	return (
 		<div>
-			<Container style={{ border: "1px red solid" }} className='p-4'>
+			<Container className='p-4'>
 				<Row className='mt-5 py-4'>
 					<h2>Welcome {"Ibrahim"}</h2>
 				</Row>
@@ -54,27 +64,67 @@ const Dashboard = () => {
 						</Card>
 					</Col>
 					<Col md={{ span: 4, offset: 3 }}>
-						<h3 className='py-3' style={{ textAlign: "center" }}>
-							You're Credit worthy, please fill in your card details to begin
-							taking loans
-						</h3>
-						<div onClick={() => setModalShow(true)} className='getStarted'>
-							<Link className='getStart' to=''>
-								Get Started
-							</Link>
-						</div>
+						{showLoan ? (
+							<div>
+								<h3 className='py-3' style={{ textAlign: "center" }}>
+									We are here for you.
+								</h3>
+								<div className='getStarted'>
+									<Link className='getStart'>Take a Loan</Link>
+								</div>
+							</div>
+						) : (
+							<div>
+								<h3 className='py-3' style={{ textAlign: "center" }}>
+									You're Credit worthy, please fill in your card details to
+									begin taking loans
+								</h3>
+								<div onClick={() => setModalShow(true)} className='getStarted'>
+									<Link className='getStart'>Add your card</Link>
+								</div>
+							</div>
+						)}
 					</Col>
 				</Row>
 				<DashModal
 					keyboard={false}
 					backdrop='static'
 					show={modalShow}
+					onHide={() => setModalShow(false)}
 				/>
 
 				<h4 className='text-center'>Your Loan History</h4>
+				<div>
+					<table className='historyTable'>
+						<thead className=''>
+							<tr>
+								<th>Date</th>
+								<th>Amount</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							{simulateLoanHistory.length > 0 ? (
+								simulateLoanHistory.map((loanHistory) => (
+									<tr>
+										<td>{loanHistory.name}</td>
+										<td>{loanHistory.department}</td>
+										<td>{loanHistory.role}</td>
+									</tr>
+								))
+							) : (
+								<tr>
+									<td colspan='4'>
+										<h4>You have no loan history yet.</h4>
+									</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+				</div>
 			</Container>
 		</div>
 	);
-}
- 
+};
+
 export default Dashboard;
--- src/components/Dashboard/modal.js
+++ src/components/Dashboard/modal.js
@@ -1,77 +1,89 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
-import { useHistory, Redirect } from "react-router";
+import Toast from "react-bootstrap/Toast";
+
+// import { useHistory, Redirect } from "react-router";
 
 const DashModal = (props) => {
-  const history = useHistory();
-  
-  const [isLoading, setLoading] = useState(props.show);
-  
-	function simulateNetworkRequest() {
-		return new Promise((resolve) => {
-			setTimeout(resolve, 5000);
-		});
-	}
+	// const history = useHistory();
+
+	const [isLoading, setLoading] = useState(false);
 
 	
 
-	useEffect(() => {
-		const load = async () => {
-			if (!isLoading) {
-				await simulateNetworkRequest();
-				setLoading(!isLoading);
-			}
-		};
-		load();
-  }, [isLoading]);
-  
-  const handleSubmit = () => {
-		setLoading(!isLoading)
-	};
 	return (
-		<Modal
-      {...props}
-      hide={()=>!isLoading}
-			size='md'
-			aria-labelledby='contained-modal-title-vcenter'
-			centered>
-			<Modal.Header closeButton>
-				<Modal.Title id='contained-modal-title-vcenter'>
-					<h4 className='text-center'>Add your card Details</h4>
-				</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				<Form>
-					<Form.Group controlId='formGroupEmail'>
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type='email' placeholder='Enter email' />
-					</Form.Group>
-					<Form.Row>
-						<Col>
-							<Form.Control placeholder='First name' />
-						</Col>
-						<Col>
-							<Form.Control placeholder='Last name' />
-						</Col>
-					</Form.Row>
-				</Form>
-			</Modal.Body>
-			<Modal.Footer>
-				{/* <Button variant='secondary' onClick={props.onHide}>
+		<div>
+			<Modal
+				{...props}
+				size='md'
+				aria-labelledby='contained-modal-title-vcenter'
+				centered>
+				<Modal.Header closeButton>
+					<Modal.Title id='contained-modal-title-vcenter'>
+						<h4 className='text-center'>Add your card Details</h4>
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form>
+						<Form.Group controlId='formGroupEmail'>
+							<Form.Label>Email address</Form.Label>
+							<Form.Control type='email' placeholder='Enter email' />
+						</Form.Group>
+						<Form.Row>
+							<Col>
+								<Form.Control placeholder='First name' />
+							</Col>
+							<Col>
+								<Form.Control placeholder='Last name' />
+							</Col>
+						</Form.Row>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					{/* <Button variant='secondary' onClick={props.onHide}>
 					Close
 				</Button> */}
-				<Button
-					disabled={!isLoading}
-					onClick={isLoading ? handleSubmit : null}
-					variant='flat'
-					className='btn-flat'>
-					{!isLoading ? "Please wait" : "Add Card"}
-				</Button>
-			</Modal.Footer>
-		</Modal>
+					<Button
+						onClick={() => {
+							props.onHide();
+							setLoading(true);
+						}}
+						variant='flat'
+						className='btn-flat'>
+						Add Card
+					</Button>
+				</Modal.Footer>
+			</Modal>
+			{isLoading && (
+				<div
+					aria-live='polite'
+					aria-atomic='true'
+					style={{
+						position: "absolute",
+						minHeight: "100px",
+						minWidth: "300px",
+						top: 30,
+						right: 230,
+					}}>
+					<Toast
+						delay={4000}
+						autohide
+						show={isLoading}
+						onClose={() => setLoading(false)}
+						style={{
+							position: "absolute",
+							top: 0,
+							right: 0,
+						}}>
+						<Toast.Header>Successfull!</Toast.Header>
+						<Toast.Body>Card added successfully</Toast.Body>
+					</Toast>
+				</div>
+			)}
+		</div>
 	);
 };
 
